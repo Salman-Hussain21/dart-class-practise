df055fc433d41395a389570f3d23ecad08d8f060
unknown <Administrator@LAB8-1.labs.org>
2025-02-17 16:36:32 +0500

    commiting project

---
+++ bin/practise.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+void main() {
+  //
+  // var marks = [ 10, 8, 7 , 6 ,8 ];
+  // print(marks);
+  // print(marks.length);
+  //
+  // int sum = 0;
+  // for ( int num in marks){
+  //   sum += num;
+  // }
+  //
+  // print("sum is = $sum");
+  //
+  // var avg = sum/marks.length;
+  // print("average is = $avg");
+  //
+  // print(" The sum is ${marks.reduce((a,b)=>a+b)}");
+  // print(" The average is ${marks.reduce((a,b)=>a+b)/marks.length}");
+  // print(" The highest marks is is ${marks.reduce((a,b)=>a>b? a:b)}");
+  // print(" The lowest marks is is ${marks.reduce((a,b)=>a<b? a:b)}");
+  //
+  //
+  // print("Enter your Name");
+  // String? message = stdin.readLineSync();
+  // print("Your message is: ${message!.toUpperCase()}");
+  //
+  // print("Enter your Age");
+  // String? age = stdin.readLineSync();
+  // print("Your age is: $age");
+  //
+  // stdout.write("The Name is ${message!.toUpperCase()}");
+  // stdout.write("& The Age is $age");
+  //
+
+
+
+}
+
---
+++ test/practise_test.dart
@@ -0,0 +1,8 @@
+import 'package:practise/practise.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
